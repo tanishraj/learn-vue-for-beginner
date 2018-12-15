842181c3e758bddc48f9f82f4e761e8a4a7254b8
tanishraj <tanishraj.quitegrowth@gmail.com>
2018-12-16 03:24:08 +0530

    Few more example

---
+++ 3-Conditional/app.js
@@ -0,0 +1,8 @@
+var app = new Vue({
+	el: '#app',
+	data: {
+		seen: true // The message will appear
+	}
+})
+
+app.seen = false; // The message will disappear
---
+++ 4-Loops/app.js
@@ -0,0 +1,12 @@
+var app = new Vue({
+	el: '#app',
+	data: {
+		todos: [
+			{text: 'Learn HTML'},
+			{text: 'Learn CSS'},
+			{text: 'Learn Javascript'},
+			{text: 'Learn Vue JS'},
+			{text: 'Learn React JS'}
+		]
+	}
+})
