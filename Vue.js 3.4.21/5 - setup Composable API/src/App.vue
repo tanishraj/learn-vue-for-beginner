20d9d7993142f9407d81ee87f473bbb0c879e131
tanishraj <learn.tanishraj@gmail.com>
2024-06-07 15:02:51 +0530

    feat: composable API setup

---
+++ Vue.js 3.4.21/5 - setup Composable API/src/App.vue
@@ -0,0 +1,34 @@
+<script>
+import ComposableApp from "@/ComposableApp.vue";
+
+export default {
+  name: "App",
+  components: {
+    ComposableApp
+  },
+  data() {
+    return {
+      message: "Hello Traditional Vue Component"
+    };
+  },
+  methods: {
+    changeMessage() {
+      this.message = "Ugggghh, Vue 2.0 sucks!!!";
+    }
+  }
+};
+</script>
+
+<template>
+  <h1>{{ message }}</h1>
+  <button v-on:click="changeMessage">
+    Click me to change greeting message
+  </button>
+  <ComposableApp />
+</template>
+
+<style scoped>
+h1 {
+  color: red;
+}
+</style>
---
+++ Vue.js 3.4.21/5 - setup Composable API/src/ComposableApp.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref } from "vue";
+
+const message = ref("Hello Vue Component using Composable API");
+
+const changeMessage2 = () => {
+  message.value = "Vue 3.0 Rocks!!!";
+};
+</script>
+
+<template>
+  <h1>{{ message }}</h1>
+  <button @click="changeMessage2">Click Me to change the message</button>
+</template>
+
+<style scoped>
+h1 {
+  color: red;
+}
+</style>
