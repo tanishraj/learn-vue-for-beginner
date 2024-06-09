6f242d23376bcbb2910ca1f262f60ad2bd571c05
tanishraj <learn.tanishraj@gmail.com>
2024-06-09 20:51:40 +0530

    feat: v-if / v-elseif / v-else example

---
+++ Vue.js 3.4.21/10 - Conditional Rendering/src/components/IfElseIfAndElse.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { ref } from 'vue'
+
+const showMessage = ref(false)
+const type = ref(['A', 'B', 'C'])
+const count = ref(0)
+
+const handleShowMessageClick = () => {
+  showMessage.value = !showMessage.value
+
+  if (count.value === 2) {
+    count.value = 0
+  } else {
+    count.value++
+  }
+}
+
+</script>
+
+<template>
+  <h3>v-if / v-else-if / v-else</h3>
+  <h1 v-if="type[count] === 'A'">A</h1>
+  <h1 v-else-if="type[count] === 'B'">B</h1>
+  <h1 v-else-if="type[count] === 'C'">C</h1>
+  <h1 v-else>Not A/B/C</h1>
+
+  <button v-on:click="handleShowMessageClick">Click me to show {{ type[count === 2 ? 0 : count + 1] }}</button>
+</template>
+
+<style scoped></style>
