994c8253c8dfe0c26f8e96608b15acd40db21ae2
tanishraj <learn.tanishraj@gmail.com>
2024-06-09 20:51:55 +0530

    feat: v-if with template element example

---
+++ Vue.js 3.4.21/10 - Conditional Rendering/src/components/IfOnTemplate.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { ref } from 'vue'
+const ok = ref(false)
+</script>
+
+<template>
+  <button @click="ok = !ok">Toggle</button>
+
+  <template v-if="ok">
+    <h1>Title</h1>
+    <p>Paragraph 1</p>
+    <p>Paragraph 2</p>
+  </template>
+</template>
+
+<style scoped></style>
