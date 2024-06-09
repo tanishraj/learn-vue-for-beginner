6509ba9d486c158a62aff9e704f06e1aa175660d
tanishraj <learn.tanishraj@gmail.com>
2024-06-09 20:52:07 +0530

    feat: v-show example

---
+++ Vue.js 3.4.21/10 - Conditional Rendering/src/components/VShowExample.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { ref } from 'vue'
+const ok = ref(false)
+</script>
+
+<template>
+  <button @click="ok = !ok">Toggle</button>
+
+  <div v-show="ok">
+    <h1>Title</h1>
+    <p>Paragraph 1</p>
+    <p>Paragraph 2</p>
+  </div>
+</template>
+
+<style scoped></style>
