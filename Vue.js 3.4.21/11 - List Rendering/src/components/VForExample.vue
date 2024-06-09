eca4972895728d9bdc9775b9eead936a280d98d4
tanishraj <learn.tanishraj@gmail.com>
2024-06-10 02:24:07 +0530

    feat: v-for example

---
+++ Vue.js 3.4.21/11 - List Rendering/src/components/VForExample.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from 'vue';
+
+const items = ref([
+  { id: 1, name: 'Item 1' },
+  { id: 2, name: 'Item 2' },
+  { id: 3, name: 'Item 3' },
+]);
+</script>
+
+<template>
+  <div class="content">
+    <div>
+      <ul>
+        <li v-for="item in items" :key="item.id">{{ item.name }}</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
