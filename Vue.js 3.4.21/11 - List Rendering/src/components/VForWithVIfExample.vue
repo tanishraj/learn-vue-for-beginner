66e6a424707b598479c5e7cc47a848d64740f5fb
tanishraj <learn.tanishraj@gmail.com>
2024-06-10 02:25:39 +0530

    feat: v-for with v-if example

---
+++ Vue.js 3.4.21/11 - List Rendering/src/components/VForWithVIfExample.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+const items = [
+  { id: 1, name: 'Item 1', isVisible: true },
+  { id: 2, name: 'Item 2', isVisible: false },
+  { id: 3, name: 'Item 3', isVisible: true },
+]
+</script>
+
+<template>
+  <div class="content">
+    <div v-for="item in items" :key="item.id">
+      <div v-if="item.isVisible">
+        {{ item.name }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
