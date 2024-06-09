6cd71692891f0771937a198aa7d1f214e70b50bb
tanishraj <learn.tanishraj@gmail.com>
2024-06-09 20:51:15 +0530

    feat: IfElse Example

---
+++ Vue.js 3.4.21/10 - Conditional Rendering/src/components/IfElseExample.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { ref } from 'vue'
+const awesome = ref(false)
+</script>
+
+<template>
+  <h3>Example for v-if and v-else:</h3>
+
+  <button @click="awesome = !awesome">Toggle</button>
+  <h1 v-if="awesome">Vue is awesome!</h1>
+  <h1 v-else>Oh no 😢</h1>
+</template>
+
+<style scoped></style>
