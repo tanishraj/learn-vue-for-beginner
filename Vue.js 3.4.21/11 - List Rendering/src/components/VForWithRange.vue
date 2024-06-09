cd200d50805b7c3642e7e2582f9902d2545469c8
tanishraj <learn.tanishraj@gmail.com>
2024-06-10 02:25:01 +0530

    feat: v-for with range example

---
+++ Vue.js 3.4.21/11 - List Rendering/src/components/VForWithRange.vue
@@ -0,0 +1,14 @@
+<script setup>
+</script>
+
+<template>
+  <div class="content">
+    <div>
+      <ul>
+        <li v-for="i in 10" :key="i">{{ i }}</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
