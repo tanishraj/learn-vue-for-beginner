e2d8dac4b28f3fbf6ca9dd02946a36a53fd5d3cb
tanishraj <learn.tanishraj@gmail.com>
2024-06-10 02:24:42 +0530

    feat: v-for with object

---
+++ Vue.js 3.4.21/11 - List Rendering/src/components/VForWithObject.vue
@@ -0,0 +1,19 @@
+<script setup>
+const myObject = {
+  property1: 'value1',
+  property2: 'value2',
+  property3: 'value3'
+}
+</script>
+
+<template>
+  <div class="content">
+    <div>
+      <ul>
+        <li v-for="(item, index) in myObject" :key="index">{{ item }}</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
