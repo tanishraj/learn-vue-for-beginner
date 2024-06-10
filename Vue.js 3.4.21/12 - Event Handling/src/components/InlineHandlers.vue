90e312e55edd1ba2f0d7ef3001b03b6df4c1411c
tanishraj <learn.tanishraj@gmail.com>
2024-06-11 00:38:37 +0530

    feat: inline handlers

---
+++ Vue.js 3.4.21/12 - Event Handling/src/components/InlineHandlers.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+</script>
+
+<template>
+  <button @click="count++">Add 1</button>
+  <p>Count is: {{ count }}</p>
+</template>
+
+<style scoped></style>
