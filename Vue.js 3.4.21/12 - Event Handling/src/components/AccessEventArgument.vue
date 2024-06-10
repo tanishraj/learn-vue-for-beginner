b01b5e89f89ed50652a8bcc9bd2a16545c0aa99c
tanishraj <learn.tanishraj@gmail.com>
2024-06-11 00:38:26 +0530

    feat: Access Event Argument

---
+++ Vue.js 3.4.21/12 - Event Handling/src/components/AccessEventArgument.vue
@@ -0,0 +1,30 @@
+<script setup>
+// import { ref } from 'vue'
+
+// const name = ref('');
+
+const logValue = (event) => {
+  console.log(event.target.value);
+}
+
+</script>
+
+<template>
+  <div class="content">
+    <label for="name">
+      <input type="text" id="name" @input="logValue" placeholder="Enter your name">
+    </label>
+
+    <label for="email">
+      <input type="text" id="email" @input="($event) => console.log($event.target.value)"
+        placeholder="Enter your email">
+    </label>
+
+    <label for="message">
+      <input type="text" id="message" @input="($event) => console.log($event.target.value)"
+        placeholder="Enter your message">
+    </label>
+  </div>
+</template>
+
+<style scoped></style>
