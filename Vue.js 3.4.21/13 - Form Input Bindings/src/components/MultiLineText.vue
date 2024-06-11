7333e6fc2904c75975021ce4ba4def0460756fae
tanishraj <learn.tanishraj@gmail.com>
2024-06-12 00:09:21 +0530

    feat: Multi Line Text example

---
+++ Vue.js 3.4.21/13 - Form Input Bindings/src/components/MultiLineText.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { ref } from 'vue';
+
+const message = ref('');
+</script>
+
+<template>
+  <span>Multiline message is:</span>
+  <p style="white-space: pre-line;">{{ message }}</p>
+  <textarea v-model="message" placeholder="add multiple lines"></textarea>
+</template>
+
+<style scoped></style>
