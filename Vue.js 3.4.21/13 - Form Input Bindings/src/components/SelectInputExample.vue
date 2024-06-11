229fabf2bbd0b6e12d47fc15e12813e6a0596887
tanishraj <learn.tanishraj@gmail.com>
2024-06-12 00:10:11 +0530

    feat: Select Input Example

---
+++ Vue.js 3.4.21/13 - Form Input Bindings/src/components/SelectInputExample.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref } from 'vue';
+
+const selected = ref('One');
+</script>
+
+<template>
+  <div>Selected: {{ selected }}</div>
+
+  <select v-model="selected">
+    <option disabled value="">Please select one</option>
+    <option value="A">A</option>
+    <option value="B">B</option>
+    <option value="C">C</option>
+  </select>
+</template>
+
+<style scoped></style>
