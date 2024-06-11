b74e26a5856ff558a8c74de800b09aba1b7ddde1
tanishraj <learn.tanishraj@gmail.com>
2024-06-12 00:08:14 +0530

    feat: checkbox Example

---
+++ Vue.js 3.4.21/13 - Form Input Bindings/src/components/CheckBoxExample.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref } from 'vue';
+
+const checkedNames = ref([]);
+</script>
+
+<template>
+  <div>Checked names: {{ checkedNames }}</div>
+
+  <input type="checkbox" id="jack" value="Jack" v-model="checkedNames">
+  <label for="jack">Jack</label>
+
+  <input type="checkbox" id="john" value="John" v-model="checkedNames">
+  <label for="john">John</label>
+
+  <input type="checkbox" id="mike" value="Mike" v-model="checkedNames">
+  <label for="mike">Mike</label>
+</template>
+
+<style scoped></style>
