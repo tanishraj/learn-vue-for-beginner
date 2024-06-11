3e338cc1aaf9975bd6ad5db134855cf958413d98
tanishraj <learn.tanishraj@gmail.com>
2024-06-12 00:09:00 +0530

    feat: Manual Value Binding

---
+++ Vue.js 3.4.21/13 - Form Input Bindings/src/components/ManualValueBinding.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { ref } from 'vue';
+
+const text = ref('');
+</script>
+
+<template>
+  <input type="text" :value="text" @input="event => text = event.target.value" />
+  <p>Input value is: {{ text }}</p>
+</template>
+
+<style scoped></style>
