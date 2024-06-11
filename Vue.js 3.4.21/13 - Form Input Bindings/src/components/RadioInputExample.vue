250b85d4469d9ac583b1d7df780bba831cc86401
tanishraj <learn.tanishraj@gmail.com>
2024-06-12 00:09:58 +0530

    feat: Radio INput Example

---
+++ Vue.js 3.4.21/13 - Form Input Bindings/src/components/RadioInputExample.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from 'vue';
+
+const picked = ref('One');
+</script>
+
+<template>
+  <div>Picked: {{ picked }}</div>
+
+  <input type="radio" id="one" value="One" v-model="picked" />
+  <label for="one">One</label>
+
+  <input type="radio" id="two" value="Two" v-model="picked" />
+  <label for="two">Two</label>
+</template>
+
+<style scoped></style>
