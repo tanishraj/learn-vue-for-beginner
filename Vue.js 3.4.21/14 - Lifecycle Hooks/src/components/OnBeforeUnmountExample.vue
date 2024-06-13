c8717b6c23ba37a508bdcd2f677faed082b7befd
tanishraj <learn.tanishraj@gmail.com>
2024-06-13 07:42:04 +0530

    feat: onBeforeUnmount Example

---
+++ Vue.js 3.4.21/14 - Lifecycle Hooks/src/components/OnBeforeUnmountExample.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { ref, onMounted, onBeforeUnmount } from 'vue';
+
+const width = ref(window.innerWidth);
+const height = ref(window.innerHeight);
+
+const updateSize = () => {
+  width.value = window.innerWidth;
+  height.value = window.innerHeight;
+};
+
+onMounted(() => {
+  window.addEventListener('resize', updateSize);
+});
+
+onBeforeUnmount(() => {
+  window.removeEventListener('resize', updateSize);
+});
+</script>
+
+<template>
+  <h3>OnBeforeUnmount() Example:</h3>
+
+  <div class="content">
+    <h1>Window Size</h1>
+    <p>Width: {{ width }}</p>
+    <p>Height: {{ height }}</p>
+  </div>
+</template>
+
+<style scoped></style>
