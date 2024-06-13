0b09462837aa65cebd67083851dbd9e053ff06c5
tanishraj <learn.tanishraj@gmail.com>
2024-06-13 07:43:06 +0530

    feat: onUnmounted Example

---
+++ Vue.js 3.4.21/14 - Lifecycle Hooks/src/components/OnUnmountedExample.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { ref, onMounted, onUnmounted } from 'vue';
+
+const seconds = ref(0);
+let timer;
+
+const updateTimer = () => {
+  seconds.value++;
+};
+
+onMounted(() => {
+  timer = setInterval(updateTimer, 1000);
+});
+
+onUnmounted(() => {
+  clearInterval(timer);
+});
+</script>
+
+<template>
+  <h3>OnUnmounted() Example:</h3>
+
+  <div class="content">
+    <h1>Timer</h1>
+    <p>Seconds: {{ seconds }}</p>
+  </div>
+</template>
+
+<style scoped></style>
