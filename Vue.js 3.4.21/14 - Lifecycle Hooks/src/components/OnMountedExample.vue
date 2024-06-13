4115d74fac6c888c50a91d98776af3a9eefc0633
tanishraj <learn.tanishraj@gmail.com>
2024-06-13 07:42:51 +0530

    feat: onMounted Example

---
+++ Vue.js 3.4.21/14 - Lifecycle Hooks/src/components/OnMountedExample.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { reactive, onMounted } from 'vue';
+
+const posts = reactive([]);
+
+onMounted(() => {
+  fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(response => response.json())
+    .then(data => {
+      posts.push(...data);
+    });
+});
+</script>
+
+<template>
+  <h3>OnMounted() Example:</h3>
+
+  <div v-for="(post, index) in posts" :key="post.id">
+    <div v-if="index < 5" class="content">
+      <h4>{{ post.title }}</h4>
+      <p>{{ post.body }}</p>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
