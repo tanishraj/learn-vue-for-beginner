c7ad992d8a9017c63a368a1574f22181a7add720
tanishraj <learn.tanishraj@gmail.com>
2024-06-14 00:52:34 +0530

    feat: watch example

---
+++ Vue.js 3.4.21/16 - Watchers/src/components/WatchExample.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { reactive, onMounted, ref, watch } from 'vue';
+
+const usersList = reactive([]);
+const searchKeyword = ref('');
+const filteredList = ref([]);
+const loading = ref(true)
+
+watch(searchKeyword, (newKeyword, oldKeyword) => {
+  filteredList.value = usersList.filter(user =>
+    user.name.toLowerCase().includes(newKeyword.toLowerCase())
+  )
+}, { immediate: true })
+
+onMounted(() => {
+  fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(data => {
+      usersList.push(...data);
+      filteredList.value.push(...data);
+    }).catch(error => {
+      console.log(error)
+    })
+    .finally(() => {
+      loading.value = false
+    })
+})
+</script>
+
+<template>
+  <div class="content">
+    <h3>Users List</h3>
+    <input type="text" v-model="searchKeyword" placeholder="Search by name" />
+    <p v-if="loading">Loading...</p>
+    <ul v-if="filteredList.length">
+      <li v-for="user in filteredList">{{ user.name }}</li>
+    </ul>
+    <p v-else>No results found</p>
+  </div>
+</template>
