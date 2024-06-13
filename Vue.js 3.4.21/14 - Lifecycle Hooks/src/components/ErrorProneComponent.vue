e07e3f05c2ae5aa8d6b5ffddc5a17aa975ddfaf7
tanishraj <learn.tanishraj@gmail.com>
2024-06-13 07:41:31 +0530

    onErrorCaptured hook example

---
+++ Vue.js 3.4.21/14 - Lifecycle Hooks/src/components/ErrorProneComponent.vue
@@ -0,0 +1,12 @@
+<script setup>
+function causeError() {
+  throw new Error('An intentional error has occurred!');
+}
+</script>
+
+<template>
+  <div>
+    <h2>Error-Prone Component</h2>
+    <button @click="causeError">Cause Error</button>
+  </div>
+</template>
