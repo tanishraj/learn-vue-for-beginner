83e102720a397713471a8838828cde5e2fa5b49e
tanishraj <learn.tanishraj@gmail.com>
2024-06-15 04:46:27 +0530

    feat: Button Component

---
+++ Vue.js 3.4.21/17 - Todo App/src/common/Button.vue
@@ -0,0 +1,23 @@
+<script setup>
+const { label } = defineProps(['label']);
+defineEmits(['onClick']);
+</script>
+
+<template>
+  <button @click="$emit('onClick')">{{ label }}</button>
+</template>
+
+<style scoped>
+button {
+  padding: 10px;
+  border-radius: 5px;
+  background-color: black;
+  color: white;
+  font-weight: 700;
+  font-size: 1rem;
+  border: none;
+  min-width: 70px;
+  max-width: 100%;
+  border: 0;
+}
+</style>
