e6f8ce6cd25aac40cc90633d0c08b84d092c1f97
tanishraj <learn.tanishraj@gmail.com>
2024-06-15 04:47:17 +0530

    feat: TodoList component implmentation

---
+++ Vue.js 3.4.21/17 - Todo App/src/components/TodoList.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { computed, watchEffect } from 'vue';
+import TodoItem from './TodoItem.vue';
+const { todoList } = defineProps(['todoList']);
+
+defineEmits(['removeTodo']);
+</script>
+
+<template>
+  <div>
+    <ul v-if="todoList && todoList.length" class="todo-list">
+      <TodoItem v-for="todo in todoList" :key="todo.id" :todoItem="todo" @removeTodo="$emit('removeTodo', $event)" />
+    </ul>
+    <p v-else>No todos</p>
+  </div>
+</template>
+
+<style scoped>
+.todo-list {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+}
+</style>
