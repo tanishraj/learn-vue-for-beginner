ebc3b279da57e1eeb2f3b8e28e0f44f0960cd333
tanishraj <learn.tanishraj@gmail.com>
2024-06-15 04:46:00 +0530

    feat: Todo App project setup

---
+++ Vue.js 3.4.21/17 - Todo App/src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import TodoForm from './components/TodoForm.vue';
+import TodoList from './components/TodoList.vue';
+</script>
+
+<template>
+  <div class="app-container">
+    <h1 class="title">Todo App</h1>
+    <TodoForm />
+  </div>
+</template>
+
+<style scoped>
+.app-container {
+  max-width: 600px;
+  margin: 0 auto;
+  background-color: #eee;
+  padding: 20px;
+}
+
+.title {
+  text-align: center;
+  margin: 0 0 20px;
+  color: black
+}
+</style>
