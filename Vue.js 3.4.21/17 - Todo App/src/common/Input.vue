7c62e9f8497edcafb0bfca3d1d59735d6aed3c6f
tanishraj <learn.tanishraj@gmail.com>
2024-06-15 04:46:39 +0530

    feat: Custom Input component

---
+++ Vue.js 3.4.21/17 - Todo App/src/common/Input.vue
@@ -0,0 +1,21 @@
+<script setup>
+const model = defineModel();
+</script>
+
+<template>
+  <input type="text" v-model="model" placeholder="Enter the text..." />
+</template>
+
+<style scoped>
+input {
+  padding: 10px;
+  border-radius: 5px;
+  border: none;
+  min-width: 70px;
+  max-width: 100%;
+  font-weight: 500;
+  font-size: 1rem;
+  outline: none;
+  border: 0;
+}
+</style>
