71fd550f253b4ed291e5f89db66b93a1d48b0d74
tanishraj <learn.tanishraj@gmail.com>
2024-06-16 12:26:03 +0530

    feat: AccessRef Example

---
+++ Vue.js 3.4.21/18 - Template Refs/src/components/AccessRefsExample.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref, onMounted } from 'vue';
+
+const input = ref(null);
+
+onMounted(() => {
+  input.value.value = "Tanish"
+});
+</script>
+
+<template>
+  <div class="content">
+    <input ref="input" />
+  </div>
+</template>
+
+<style scoped></style>
