cf7b03c5b099399cf2c801328a379e1ac61c8f4a
tanishraj <learn.tanishraj@gmail.com>
2024-06-16 12:26:21 +0530

    feat: Access ref with v-for example

---
+++ Vue.js 3.4.21/18 - Template Refs/src/components/AccessRefsWithVForExample.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref, onMounted } from 'vue';
+
+const inputs = ref([]);
+
+onMounted(() => {
+  inputs.value[0].value = "Tanish";
+});
+</script>
+
+<template>
+  <div class="content">
+    <div v-for="i in 5" :key="i">
+      <input ref="inputs" placeholder="Enter Name" />
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
