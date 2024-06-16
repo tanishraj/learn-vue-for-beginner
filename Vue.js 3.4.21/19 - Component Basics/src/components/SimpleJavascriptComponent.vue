0bbb2becb36ba108edc83e354691b8bf64bf3b68
tanishraj <learn.tanishraj@gmail.com>
2024-06-17 00:35:04 +0530

    feat: simple javascript example

---
+++ Vue.js 3.4.21/19 - Component Basics/src/components/SimpleJavascriptComponent.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  // component logic here
+  name: 'SimpleJavascriptComponent',
+}
+</script>
+
+<template>
+  <!-- html here -->
+  <div class="content">
+    <h3>Simple Javascript Component</h3>
+  </div>
+</template>
+
+<style scoped>
+/* Define you component level styles here */
+
+h3 {
+  color: blueviolet;
+}
+</style>
