98beb6dd93572bd71219824f199fe2d86fc9edc8
tanishraj <learn.tanishraj@gmail.com>
2024-06-17 00:36:04 +0530

    feat: Passing props to child example

---
+++ Vue.js 3.4.21/19 - Component Basics/src/components/ChildComponentProps.vue
@@ -0,0 +1,20 @@
+<script setup>
+defineProps({
+  title: String,
+  message: String
+})
+</script>
+
+<template>
+  <div class="content">
+    <h2>{{ title }}</h2>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<style scoped>
+/* Add any component-specific styles here */
+h2 {
+  color: blue;
+}
+</style>
---
+++ Vue.js 3.4.21/19 - Component Basics/src/components/PassingPropsExample.vue
@@ -0,0 +1,18 @@
+<script setup>
+// component logic here
+import ChildComponentProps from './ChildComponentProps.vue';
+
+</script>
+
+<template>
+  <!-- html here -->
+  <h2>Passing Props from Parent To Child:</h2>
+  <div class="content">
+    <h3>Parent Component</h3>
+    <ChildComponentProps title="Hello from Parent" message="This is a message passed from the parent component." />
+  </div>
+</template>
+
+<style scoped>
+/* Define you component level styles here */
+</style>
