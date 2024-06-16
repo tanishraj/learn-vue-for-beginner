b7d68d0dd0b2461789bf35ddf01f8ff35b4b3b07
tanishraj <learn.tanishraj@gmail.com>
2024-06-16 12:27:14 +0530

    feat: Ref on component example

---
+++ Vue.js 3.4.21/18 - Template Refs/src/components/RefOnComponentExample.vue
@@ -0,0 +1,24 @@
+<script setup>
+// Todo: Look into this example
+import { ref } from 'vue';
+import ChildComponent from './ChildComponent.vue';
+
+const childComp = ref(null);
+
+const focusChildInput = () => {
+  if (childComp.value) {
+    childComp.value.focusInput();
+  }
+};
+
+</script>
+
+<template>
+  <div class="content">
+    <h3>Parent Component</h3>
+    <ChildComponent ref="childComp" />
+    <button @click="focusChildInput">Focus Child Input</button>
+  </div>
+</template>
+
+<style scoped></style>
