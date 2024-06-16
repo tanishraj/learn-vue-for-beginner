00c9d0f9e47bf59cb56b30e75212e463d2ff94ef
tanishraj <learn.tanishraj@gmail.com>
2024-06-17 00:35:30 +0530

    feat: simepl composable component example

---
+++ Vue.js 3.4.21/19 - Component Basics/src/components/SimpleComposableComponent.vue
@@ -0,0 +1,18 @@
+<script setup>
+// component logic here
+</script>
+
+<template>
+  <!-- html here -->
+  <div class="content">
+    <h3>Simple Composable Component</h3>
+  </div>
+</template>
+
+<style scoped>
+/* Define you component level styles here */
+
+h3 {
+  color: cadetblue;
+}
+</style>
