596631af8ce25568a36510d94c255dd7efa586eb
tanishraj <learn.tanishraj@gmail.com>
2024-06-17 00:36:47 +0530

    feat: child to parent props example

---
+++ Vue.js 3.4.21/19 - Component Basics/src/components/ChildComponentC2P.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref, defineProps, defineEmits } from 'vue';
+
+const childData = ref('Child Data');
+
+defineProps({
+  title: String,
+  message: String
+})
+
+defineEmits(['updateTitle'])
+</script>
+
+<template>
+  <div class="content">
+    <h2>{{ title }}</h2>
+    <p>{{ message }}</p>
+    <button @click="$emit('updateTitle', childData)">Update Parent Component Title</button>
+  </div>
+</template>
+
+<style scoped>
+/* Add any component-specific styles here */
+h2 {
+  color: blue;
+}
+</style>
---
+++ Vue.js 3.4.21/19 - Component Basics/src/components/ChildToParentExample.vue
@@ -0,0 +1,27 @@
+<script setup>
+// component logic here
+import { ref } from 'vue';
+import ChildComponentC2P from './ChildComponentC2P.vue';
+
+const parentTitle = ref('');
+
+const updateTitle = (title) => {
+  console.log("HELLO PARENT")
+  parentTitle.value = title;
+}
+</script>
+
+<template>
+  <!-- html here -->
+  <h2>Passing Props from Child To Parent:</h2>
+  <div class="content">
+    <h3>Parent Component</h3>
+    <p>Data to come from child component: {{ parentTitle }}</p>
+    <ChildComponentC2P title="Hello from Parent" message="This is a message passed from the parent component."
+      @updateTitle="updateTitle" />
+  </div>
+</template>
+
+<style scoped>
+/* Define you component level styles here */
+</style>
