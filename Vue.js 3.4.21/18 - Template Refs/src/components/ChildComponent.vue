7af6d4c159fdb2b56c00a1f95fd812be51cee212
tanishraj <learn.tanishraj@gmail.com>
2024-06-16 12:26:46 +0530

    feat: child component implementation

---
+++ Vue.js 3.4.21/18 - Template Refs/src/components/ChildComponent.vue
@@ -0,0 +1,18 @@
+<script setup>
+function clearInput() {
+  inputField.value = '';
+},
+function focusInput() {
+  inputField.focus();
+},
+</script>
+
+<template>
+  <div class="content">
+    <h3>Child Component</h3>
+    <input ref="inputField" placeholder="Type something..." />
+    <button @click="clearInput">Clear Input</button>
+  </div>
+</template>
+
+<style scoped></style>
