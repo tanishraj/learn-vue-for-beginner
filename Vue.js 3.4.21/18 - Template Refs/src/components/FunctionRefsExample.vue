fe1b8dca45585d5af8188c991df7232878fb1815
tanishraj <learn.tanishraj@gmail.com>
2024-06-16 12:26:59 +0530

    feat: function ref example

---
+++ Vue.js 3.4.21/18 - Template Refs/src/components/FunctionRefsExample.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref, onMounted } from 'vue';
+
+const input = ref(null);
+
+onMounted(() => {
+  input.value.value = "Tanish"
+});
+</script>
+
+<template>
+  <div class="content">
+    <input :ref="(el) => { input = el }" />
+  </div>
+</template>
+
+<style scoped></style>
