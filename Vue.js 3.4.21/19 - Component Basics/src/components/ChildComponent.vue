4e4342365c43c15732fd76a43deb437215a57086
tanishraj <learn.tanishraj@gmail.com>
2024-06-17 00:34:30 +0530

    feat: using component example

---
+++ Vue.js 3.4.21/19 - Component Basics/src/components/ChildComponent.vue
@@ -0,0 +1,18 @@
+<script setup>
+// logic here
+</script>
+
+<template>
+  <!-- html here -->
+  <div class="content">
+    <h3>Child Component</h3>
+  </div>
+</template>
+
+<style scoped>
+/* Define you component level styles here */
+
+h3 {
+  color: red;
+}
+</style>
---
+++ Vue.js 3.4.21/19 - Component Basics/src/components/UsingComponent.vue
@@ -0,0 +1,24 @@
+<script>
+// component logic here
+import ChildComponent from './ChildComponent.vue';
+
+export default {
+  name: 'UsingComponent',
+  components: {
+    ChildComponent
+  }
+}
+</script>
+
+<template>
+  <!-- html here -->
+  <div class="content">
+    <h3>Using a Component</h3>
+    <p>Calling ChildComponent here...</p>
+    <ChildComponent />
+  </div>
+</template>
+
+<style scoped>
+/* Define you component level styles here */
+</style>
