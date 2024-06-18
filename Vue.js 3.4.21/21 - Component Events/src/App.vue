bed6d0ceaf35e617b61a8919f181c551319f8e37
tanishraj <learn.tanishraj@gmail.com>
2024-06-19 01:50:38 +0530

    feat: Component Events Example

---
+++ Vue.js 3.4.21/21 - Component Events/src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+import EmittingAndListeningToEvents from './components/EmittingAndListeningToEvents.vue';
+import DeclaringEmittedEvents from './components/DeclaringEmittedEvents.vue';
+import EventValidation from './components/EventValidation.vue';
+</script>
+
+<template>
+  <h1>Component Events</h1>
+
+  <div class="content">
+    <h3>Emitting and Listening to Events:</h3>
+    <p>A component can emit custom events directly in template expressions (e.g. in a v-on handler) using the built-in
+      $emit method:</p>
+    <EmittingAndListeningToEvents />
+  </div>
+
+  <div class="content">
+    <h3>Declaring Emitted Events:</h3>
+    <p>A component can explicitly declare the events it will emit using the defineEmits() macro:</p>
+    <DeclaringEmittedEvents />
+  </div>
+
+  <div class="content">
+    <h3>Events Validation:</h3>
+    <p>A component can explicitly declare the events it will emit using the defineEmits() macro:</p>
+    <EventValidation />
+  </div>
+</template>
+
+<style scoped></style>
